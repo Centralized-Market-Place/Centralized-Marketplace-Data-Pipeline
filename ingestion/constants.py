f0c18dff11f0378400cdb9c0b6fcd2f10a4a7c09
million-t <milliontolessa@gmail.com>
2025-01-05 13:54:58 +0300

    add more channels

--- ingestion/constants.py
+++ ingestion/constants.py
@@ -1,8 +1,17 @@
 # CHANNEL_IDS = ["@marakibrand"]
-CHANNEL_IDS = [ "@GTechMobile",
-                "@used_phone_ethiopia_1", 
-                "@henokestezia",
-                "@Rekashstore",
-                "@birukcomputersandlaptops",
-                "@misgetechno"
-                ]
+# CHANNEL_IDS = [ "@GTechMobile",
+#                 "@used_phone_ethiopia_1", 
+#                 "@henokestezia",
+#                 "@Rekashstore",
+#                 "@birukcomputersandlaptops",
+#                 "@misgetechno"
+#                 ]
+
+
+CHANNEL_IDS = [
+    "@Shewabrand",
+    "@bros_fashionn",
+    "@brandsonlymart",
+    "@fash1onweek",
+    "@FashionforwardbyDinah"
+]
