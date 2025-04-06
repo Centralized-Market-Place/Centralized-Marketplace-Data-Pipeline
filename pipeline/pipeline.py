f11249c0a085743f18d0e63ac3ed8539ae70c570
million-t <milliontolessa@gmail.com>
2025-04-06 20:19:28 +0300

    fix imports

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from ingestion.fetcher import fetch_runner, fetch_channel_runner, fetch_all_channels_runner
-from storage.store import store_raw_data, fetch_stored_messages, update_products
+from storage.store import store_raw_data, fetch_stored_messages
 from processing.preprocessor import preprocess, extract_oldest_and_latest_ids, insert_into_products_collection
 import asyncio
 
