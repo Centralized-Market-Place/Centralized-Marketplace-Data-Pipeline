0d2497aed746902e97189f468b464ebb9e00ad7f
million-t <milliontolessa@gmail.com>
2025-05-18 22:11:27 +0300

    remove invalid imports

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,7 +4,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from ingestion.fetcher import fetch_runner, fetch_channel_runner, fetch_all_channels_runner
 from storage.store import store_raw_data, fetch_stored_messages
-from processing.preprocessor import preprocess, extract_oldest_and_latest_ids, insert_into_products_collection
 import asyncio
 
 async def run_pipeline():
